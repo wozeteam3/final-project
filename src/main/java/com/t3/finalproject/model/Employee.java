5eb9c5c6522fbc8e532ea3a81237d4c7ec9562e9
Fyrebright <aidcostello@gmail.com>
2021-08-03 14:28:33 -0400

    Add constructors

--- src/main/java/com/t3/finalproject/model/Employee.java
+++ src/main/java/com/t3/finalproject/model/Employee.java
@@ -1,29 +1,75 @@
 package com.t3.finalproject.model;
 
+/**
+ * @author Aidan
+ *
+ */
 public class Employee {
-	private int emp_id;
-	private int benefit_id;
+	private int empId;
+	private int benefitId;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String phoneNum;
+	private int payGrade;
+	private int payStep;
+	private boolean isSalaried;
 	
+	public Employee() {}
 	
-
-	public int getEmp_id() {
-		return this.emp_id;
+	/**
+	 * @param id
+	 * @param benId
+	 * @param f
+	 * @param l
+	 * @param email
+	 * @param phone
+	 * @param grade
+	 * @param step
+	 * @param salaried
+	 */
+	public Employee(int id, int benId, String f, String l, String email, 
+			String phone, int grade, int step, boolean salaried) {
+		this.empId = id;
+		this.benefitId = benId;
+		this.firstName = f;
+		this.lastName = l;
+		this.email = email;
+		this.phoneNum = phone;
+		this.payGrade = grade;
+		this.payStep = step;
+		this.isSalaried = salaried;
+	}
+	
+	/**
+	 * @param e
+	 */
+	public Employee(Employee e) {
+		this.empId = e.getEmpId();
+		this.benefitId = e.getBenefitId();
+		this.firstName = e.getFirstName();
+		this.lastName = e.getLastName();
+		this.email = e.getEmail();
+		this.phoneNum = e.getPhoneNum();
+		this.payGrade = e.getPayGrade();
+		this.payStep = e.getPayStep();
+		this.isSalaried = e.isSalaried();
+	}
+ 
+	public int getEmpId() {
+		return this.empId;
 	}
 
-	public void setEmp_id(int emp_id) {
-		this.emp_id = emp_id;
+	public void setEmpId(int empId) {
+		this.empId = empId;
 	}
 
-	public int getBenefit_id() {
-		return this.benefit_id;
+	public int getBenefitId() {
+		return this.benefitId;
 	}
 
-	public void setBenefit_id(int benefit_id) {
-		this.benefit_id = benefit_id;
+	public void setBenefitId(int benefitId) {
+		this.benefitId = benefitId;
 	}
 
 	public String getFirstName() {
@@ -81,8 +127,4 @@ public class Employee {
 	public void setSalaried(boolean isSalaried) {
 		this.isSalaried = isSalaried;
 	}
-
-	private int payGrade;
-	private int payStep;
-	private boolean isSalaried;
 }
