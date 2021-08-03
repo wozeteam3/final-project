071c6b15af5ffc3707a47d4b7afe9923f511a7a9
Fyrebright <aidcostello@gmail.com>
2021-08-03 12:27:29 -0400

    Add Employee fields

--- src/main/java/com/t3/finalproject/model/Employee.java
+++ src/main/java/com/t3/finalproject/model/Employee.java
@@ -1,5 +1,14 @@
 package com.t3.finalproject.model;
 
-public class Employee {
 
+public class Employee {
+	private int emp_id;
+	private int benefit_id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNum;
+	private int payGrade;
+	private int payStep;
+	private boolean isSalaried;
 }
