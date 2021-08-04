73301a99b5d18880a710732eefa7db149b7a191b
brianHayes1916 <briancampbellhayes@icloud.com>
2021-08-04 09:41:24 -0500

    SQL statements and createSPG
    Adds SQL queries for createSPG and increase for SPS and SPG. Adds the method for creating a SPG.

--- src/main/java/com/t3/finalproject/FinalProjectApplication.java
+++ src/main/java/com/t3/finalproject/FinalProjectApplication.java
@@ -1,5 +1,8 @@
 package com.t3.finalproject;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,27 @@ public class FinalProjectApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(FinalProjectApplication.class, args);
+    
+    final String createSPG = "INSERT INTO paygrade_salary (step0, step1, step2, step3, step4) VALUES(?, ?, ?, ?, ?)";
+    final String increaseSPS = "UPDATE employee SET pay_step = ? WHERE emp_id = ?";
+    final String increaseSPG = "UPDATE employee SET pay_grade = ? WHERE emp_id = ?";
+    
+    public void createSalaryPayGrade(int st0, int st1, int st2, int st3, int st4) {
+    	try(
+    			Connection c = db.getconnection();
+    			PreparedStatement ps = c.prepareStatement(createSPG)
+    		){
+    		ps.setString(1, st0);
+    		ps.setString(2, st1);
+    		ps.setString(3, st2);
+    		ps.setString(4, st3);
+    		ps.setString(5, st4);
+    		
+    		ps.executeUpdate();
+    	}
+    	catch(SQLException e) {
+    		e.printStackTrace();
+    	}
+    }
   }
 }
